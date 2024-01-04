112bed75587b563aa22211db80691b9cd1ee4aae
CHAIB Roufaida <chroufaida1@gmail.com>
2024-01-04 13:25:32 +0000

    Fix Bug

--- src/main/java/ma/ehei/calculatrice/App.java
+++ src/main/java/ma/ehei/calculatrice/App.java
@@ -3,5 +3,8 @@ package ma.ehei.calculatrice;
 public class App {
 	public static void main(String[] args) {
 		System.out.println(Calculatrice.add(5, 5));
+		System.out.println(Calculatrice.add(1, 5));
+		System.out.println(Calculatrice.add(2, 5));
+		System.out.println(Calculatrice.add(5, 5));
 	}
 }
